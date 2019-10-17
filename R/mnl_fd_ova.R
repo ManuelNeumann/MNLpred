c23998853719ab2e8e66dfbbcbcb658d5982e0ed
ManuelNeumann <neumann.manuel94@gmx.de>
2019-10-17 16:03:28 +0200

    Updating variable names & details

--- R/mnl_fd_ova.R
+++ R/mnl_fd_ova.R
@@ -1,6 +1,18 @@
 #' Multinomial First Differences Prediction (Observed Value Approach)
 #'
-#' @return
+#' This function predicts values for two different scenarios over a range of
+#' values. It then takes the differences between the different simulations to
+#' return first differences for each value.
+#'
+#' The function uses the \code{\link{mnl_pred_ova}} function for each scenario.
+#' The results of these predictions are also returned and can therefore be
+#' easily accessed. If you need predictions for multiple scenarios, you can use
+#' this function to both plot the predictions for each scenario and the
+#' differences between them.
+#'
+#' @param model
+#'
+#' @return The function returns a list with several elements.
 #' @export
 #'
 #' @examples
@@ -11,7 +23,7 @@ mnl_fd_ova <- function(model,
                        xvari,
                        scenname,
                        scenvalues,
-                       by = 1,
+                       by = NULL,
                        nsim = 1000,
                        seed = "random",
                        probs = c(0.025, 0.975)){
@@ -40,9 +52,9 @@ mnl_fd_ova <- function(model,
 
 
   # First differences
-  P_fd <- array(NA, dim = c(nsim, pred1$nchoices, pred1$nvariation))
+  P_fd <- array(NA, dim = c(nsim, pred1$nChoices, pred1$nVariation))
 
-  for (i in 1:pred1$nchoices) {
+  for (i in 1:pred1$nChoices) {
     P_fd[, i,] <- pred2$P[, i,] - pred1$P[, i,]
   }
 
@@ -53,8 +65,8 @@ mnl_fd_ova <- function(model,
   plotdata_fd[, c(4:6)] <- NA
 
   start <- 1
-  for (i in 1:pred1$nchoices){
-    end <- i*pred1$nvariation
+  for (i in 1:pred1$nChoices){
+    end <- i*pred1$nVariation
     plotdata_fd[c(start:end), "mean"] <- apply(P_fd[, i, ], 2, mean)
     plotdata_fd[c(start:end), "lower"] <- apply(P_fd[, i, ], 2, quantile, probs = probs[1])
     plotdata_fd[c(start:end), "upper"] <- apply(P_fd[, i, ], 2, quantile, probs = probs[2])
