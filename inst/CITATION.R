2692a4388f9f104d8a7e944b42a0dd92e70ba550
ManuelNeumann <neumann.manuel94@gmx.de>
2019-10-25 10:23:50 +0200

    Reviewed description and added citation

---
+++ inst/CITATION.R
@@ -0,0 +1,13 @@
+citHeader("To cite MNLpred in publications please use:")
+
+citEntry(entry = "Article",
+  title        = "MNLpred - Simulated Predicted Probabilities for Multinomial Logit Models",
+  author       = as.person("Manuel Neumann"),
+  year         = "2019",
+  url          = "https://github.com/ManuelNeumann/MNLpred",
+
+  textVersion  =
+  paste("Garrett Grolemund, Hadley Wickham (2011).",
+        "Dates and Times Made Easy with lubridate.",
+        "URL https://github.com/ManuelNeumann/MNLpred.")
+)
